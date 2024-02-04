f6c68a55b97d54a72481608bade88de6f302b066
blueloveTH <blueloveth@foxmail.com>
2024-02-04 11:07:17 +0800

    Update style.py

--- tinymongo/style.py
+++ tinymongo/style.py
@@ -2,27 +2,26 @@ import streamlit as st
 
 def setup_style():
     st.markdown("""
-    <style>
-        .st-emotion-cache-16txtl3 {
-            padding: 3rem 1.5rem;       // 改变sidebar的padding
-        }
-        .st-emotion-cache-phzz4j {
-            gap: 0.4rem;                // 改变sidebar按钮的间距
-        }     
+    <style>  
         div.block-container {
-            padding-top: 2rem;          // 改变main的padding
+            padding-top: 2rem;          // 改变总体的padding
+        }
+        div[data-testid="stSidebarUserContent"] {
+            padding-top: 2rem;          // 改变sidebar的padding
         }
     </style>
     """, unsafe_allow_html=True)
 
-# st.markdown("""
-#             <style>
-#                 div[data-testid="column"] {
-#                     width: fit-content !important;
-#                     flex: unset;
-#                 }
-#                 div[data-testid="column"] * {
-#                     width: fit-content !important;
-#                 }
-#             </style>
-#             """, unsafe_allow_html=True)
+    st.markdown("""
+    <style>
+        section.main {
+            div[data-testid="column"] {
+                width: fit-content !important;
+                flex: unset;
+            }
+            div[data-testid="column"] * {
+                width: fit-content !important;
+            }
+        }
+    </style>
+    """, unsafe_allow_html=True)
